99fde6023ec5df676c5486d76db97c9784dc19e1
LuisMolina <Luis.Molina1@alu.uclm.es>
2022-11-24 20:58:37 +0100

    Modificación arquitectura Cliente / Servidor
    - Issue #26
     Queda solucionada.

---
+++ IT_1_PEREGRINOS/src/main/java/ISO_BC044/IT_1_PEREGRINOS_DOMINIO/Camino.java
@@ -0,0 +1,32 @@
+package ISO_BC044.IT_1_PEREGRINOS_DOMINIO;
+public class Camino {
+
+	private int idCamino;
+	private String nombreCamino;
+	private String tipoCamino;
+	private int dificultad;
+
+	/**
+	 * 
+	 * @param idCamino
+	 * @param nombreCamino
+	 * @param tipoCamino
+	 * @param dificultad
+	 */
+	public void Camino(int idCamino, String nombreCamino, String tipoCamino, int dificultad) {
+		// TODO - implement Camino.Camino
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param nombreCamino
+	 * @param tipoCamino
+	 * @param dificultad
+	 */
+	public Camino anadirCamino(int nombreCamino, int tipoCamino, int dificultad) {
+		// TODO - implement Camino2.anadirCamino
+		throw new UnsupportedOperationException();
+	}
+
+}

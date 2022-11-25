c6cd5da9402104247a0f548de89045ad44695423
LauraToledoGutierrez <93611178+LauraToledoGutierrez@users.noreply.github.com>
2022-11-25 19:28:05 +0100

    Add files via upload
    Implementacion aplicacion peregrinos iteracion 2

---
+++ IT_2_PEREGRINOS/src/main/java/ISO_BC044/IT_2_PEREGRINOS_PRESENTACION/IU_Peregrino.java
@@ -0,0 +1,15 @@
+package ISO_BC044.IT_2_PEREGRINOS_PRESENTACION;
+
+public class IU_Peregrino {
+
+	public void clickmostraropinion() {
+		// TODO - implement IU_Peregrino.clickmostraropinion
+		throw new UnsupportedOperationException();
+	}
+
+	public void clickfiltraropiniones() {
+		// TODO - implement IU_Peregrino.clickfiltraropiniones
+		throw new UnsupportedOperationException();
+	}
+
+}

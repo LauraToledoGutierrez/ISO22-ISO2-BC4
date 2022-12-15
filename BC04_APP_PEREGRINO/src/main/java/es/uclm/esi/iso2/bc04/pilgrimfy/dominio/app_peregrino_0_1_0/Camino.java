b77d4093a607384800247ecfcb6185d2a7880fb3
AlejandroVV <72606126+alejandrovictoria99@users.noreply.github.com>
2022-12-15 19:03:57 +0100

    fase implementación
    implementación it5 + modificacion implementacion anteriores+ añadir proyecto server + .vpp del proyecto

---
+++ BC04_APP_PEREGRINO/src/main/java/es/uclm/esi/iso2/bc04/pilgrimfy/dominio/app_peregrino_0_1_0/Camino.java
@@ -0,0 +1,33 @@
+package es.uclm.esi.iso2.bc04.pilgrimfy.dominio.app_peregrino_0_1_0;
+
+public class Camino {
+
+	private int idCamino;
+	private String nombreCamino;
+	private String tipoCamino;
+	private int dificultad;
+
+	/**
+	 * 
+	 * @param idCamino
+	 * @param nombreCamino
+	 * @param tipoCamino
+	 * @param dificultad
+	 */
+	public Camino(int idCamino, String nombreCamino, String tipoCamino, int dificultad) {
+		// TODO - implement Camino.Camino
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param nombreCamino
+	 * @param tipoCamino
+	 * @param dificultad
+	 */
+	public Camino anadirCamino(int nombreCamino, int tipoCamino, int dificultad) {
+		// TODO - implement Camino.anadirCamino
+		throw new UnsupportedOperationException();
+	}
+
+}
